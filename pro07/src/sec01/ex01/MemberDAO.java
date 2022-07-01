987920edef3607fffccb03e70bea85bd09c4bc89
HyunJun123456 <juni303@chauniv.ac.kr>
2022-07-02 00:33:59 +0900

    서블릿 공부중2

--- pro07/src/sec01/ex01/MemberDAO.java
+++ pro07/src/sec01/ex01/MemberDAO.java
@@ -2,24 +2,45 @@ package sec01.ex01;
 
 import java.sql.Connection;
 import java.sql.Date;
-import java.sql.Driver;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
+
 public class MemberDAO {
-	private Statement stmt;
 	private Connection con;
+	private PreparedStatement pstmt;
+	private DataSource dataFactory;
+	
+	public MemberDAO() {
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 	
 	public List<MemberVO> listMembers(){
 	List<MemberVO> list = new ArrayList<MemberVO>();
 		try {
-		connDB();
+			
+//		connDB();
+		con = dataFactory.getConnection();
 		String query = "select * from t_member";
 		System.out.println(query);
-		ResultSet rs = stmt.executeQuery(query);
+		ResultSet rs = pstmt.executeQuery(query);
 		while(rs.next()) {
 		String id = rs.getString("id");
 		String pwd = rs.getString("pwd");
@@ -35,7 +56,7 @@ public class MemberDAO {
 		list.add(vo);
 	}
 	rs.close();
-	stmt.close();
+	pstmt.close();
 	con.close();
 		}catch (Exception e)
 		{
@@ -44,16 +65,16 @@ public class MemberDAO {
 		return list;
 }
 
-private void connDB() {
-	try {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		System.out.println("Oracle 드라이버 로딩 성공");
-		con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "123456");
-		System.out.println("Connection 생성 성공");
-		stmt = con.createStatement();
-		System.out.println("Statement 생성 성공");
-	} catch (Exception e) {
-		e.printStackTrace();
-	}
-}
+//private void connDB() {
+//	try {
+//		Class.forName("oracle.jdbc.driver.OracleDriver");
+//		System.out.println("Oracle 드라이버 로딩 성공");
+//		con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "123456");
+//		System.out.println("Connection 생성 성공");
+//		stmt = con.createStatement();
+//		System.out.println("Statement 생성 성공");
+//	} catch (Exception e) {
+//		e.printStackTrace();
+//	}
+//}
 }
