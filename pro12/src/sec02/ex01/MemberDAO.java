67ed79793c790844aaa9f6096dc7946cf06ddc9b
HyunJun123456 <juni303@chauniv.ac.kr>
2022-07-14 19:22:48 +0900

    서블릿 공부중14

---
+++ pro12/src/sec02/ex01/MemberDAO.java
@@ -0,0 +1,121 @@
+package sec02.ex01;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
+
+public class MemberDAO {
+	private PreparedStatement pstmt;
+	private Connection con;
+	private DataSource dataFactory;
+	
+	public MemberDAO() {
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List listMembers(MemberVO memberVO) {
+		List membersList = new ArrayList();
+		String _name=memberVO.getName();
+		try {
+			con = dataFactory.getConnection();
+			String query = "select * from t_member ";
+			
+			if((_name!=null && _name.length()!=0)){
+				 query+=" where name=?";
+				 pstmt = con.prepareStatement(query);
+				 pstmt.setString(1, _name);
+			}else {
+				pstmt = con.prepareStatement(query);	
+			}
+			
+			
+			System.out.println("prepareStatememt: " + query);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				MemberVO vo = new MemberVO();
+				vo.setId(id);
+				vo.setPwd(pwd);
+				vo.setName(name);
+				vo.setEmail(email);
+				vo.setJoinDate(joinDate);
+				membersList.add(vo);
+			}
+			rs.close();
+			pstmt.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return membersList;
+	}
+
+
+
+//	
+//	public void addMember(MemberVO memberVO) {
+//		try {
+//			Connection con = dataFactory.getConnection();
+//			String id = memberVO.getId();
+//			String pwd = memberVO.getPwd();
+//			String name = memberVO.getName();
+//			String email = memberVO.getEmail();
+//			
+//			String query = "insert into t_member";
+//			query += " (id, pwd, name, email)";
+//			query += " values(?, ?, ?, ?)";
+//			System.out.println("prepareStatement: "+query);
+//			pstmt = con.prepareStatement(query);
+//			pstmt.setString(1, id);
+//			pstmt.setString(2, pwd);
+//			pstmt.setString(3, name);
+//			pstmt.setString(4, email);
+//			pstmt.executeUpdate();
+//			pstmt.close();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	public void delMember(String id) {
+//		try {
+//			Connection con = dataFactory.getConnection();
+//			String query = "delete from t_member"+" where id=?";
+//			System.out.println("prepareStatement:"+query);
+//			pstmt = con.prepareStatement(query);
+//			pstmt.setString(1, id);
+//			pstmt.executeUpdate();
+//			pstmt.close();
+//			
+//			
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//		
+//	}
+//	
+
+
+}
