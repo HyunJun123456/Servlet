646be23172f4c25615dc66d9b6d3a5624f499dba
HyunJun123456 <juni303@chauniv.ac.kr>
2022-07-24 13:38:54 +0900

    서블릿 공부중 24

---
+++ pro16/src/sec02/ex01/MemberDAO.java
@@ -0,0 +1,56 @@
+package sec02.ex01;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	private Connection con;
+	private PreparedStatement pstmt;
+	private DataSource dataFactory;
+	
+	public MemberDAO() {
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean overlappedID(String id) {
+		boolean result = false;
+		try {
+			con = dataFactory.getConnection();
+			String query = " select decode(count(*),1,'true','false') as result from t_member";
+			query += " where id = ?";
+			System.out.println("prepareStatement: "+query);
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, id);
+			ResultSet rs = pstmt.executeQuery();
+			rs.next();
+			result = Boolean.parseBoolean(rs.getString("result"));
+			pstmt.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	
+	
+		
+	
+	
+}
