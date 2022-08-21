99de66b2684e0a38d861aec27ec46cadc1443325
HyunJun123456 <juni303@chauniv.ac.kr>
2022-08-21 12:06:47 +0900

    서블릿 공부 21

--- pro29/src/main/java/com/myspring/pro29/ex01/TestController.java
+++ pro29/src/main/java/com/myspring/pro29/ex01/TestController.java
@@ -2,6 +2,7 @@ package com.myspring.pro29.ex01;
 
 
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,6 +10,9 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -75,7 +79,31 @@ public class TestController {
 	  logger.info(vo.toString());
   }
   
+  @RequestMapping("/membersList2")
+  public ResponseEntity<List<MemberVO>> listMembers2(){
+	  List<MemberVO> list = new ArrayList<MemberVO>();
+	  for(int i =0; i<10; i++) {
+		  MemberVO vo = new MemberVO();
+		  vo.setId("hong"+i);
+		  vo.setPwd("123"+i);
+		  vo.setName("홍길동"+i);
+		  vo.setEmail("hong"+i+"@test.com");
+		  list.add(vo);
+		  
+	  }
+	  return new ResponseEntity(list, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
   
+  @RequestMapping(value="/res3")
+  public ResponseEntity res3() {
+		HttpHeaders responseHeaders = new HttpHeaders();
+	    responseHeaders.add("Content-Type", "text/html; charset=utf-8");
+	    String message = "<script>";
+		message += " alert('새 회원을 등록합니다.');";
+		message += " location.href='/pro29/test/membersList2'; ";
+		message += " </script>";
+		return  new ResponseEntity(message, responseHeaders, HttpStatus.CREATED);
+	}
   
   
 }
